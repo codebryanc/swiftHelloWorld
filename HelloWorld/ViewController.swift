79d7cf32e93a1fe5bc081f6f1b20a6ba2f51ff45
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-04-18 17:14:54 -0500

    First Commit => create the project and layout for Main.Storyboard => page 30

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by code.bryanc on 18/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblHelloWorldTitle: UILabel!
+    @IBOutlet weak var btnHelloWorld: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
